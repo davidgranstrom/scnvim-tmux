6d705f650c5e6072f9293dac241b60ba21784b92
David Granström <david@davidgranstrom.com>
2022-07-03 16:30:48 +0200

    Emulate post window API

--- lua/scnvim/_extensions/tmux/init.lua
+++ lua/scnvim/_extensions/tmux/init.lua
@@ -16,9 +16,34 @@ local function get_pane_id()
   return id:gsub('\n', '')
 end
 
-sclang.on_init:replace(function()
-  local path = vim.fn.tempname()
-  tmux.post_buffer = io.open(path, 'wb')
+local function is_open()
+  if not tmux.pane_id then
+    return false
+  end
+  local pane_ids = tmux_send({
+    'list-panes',
+    '-F',
+    '#{pane_id}'
+  })
+  pane_ids = vim.split(pane_ids, '\n', {trimempty = true})
+  local result = vim.tbl_filter(function(id)
+    return id == tmux.pane_id
+  end, pane_ids)
+  return #result == 1
+end
+
+function tmux.create()
+  tmux.path = vim.fn.tempname()
+  tmux.post_buffer = io.open(tmux.path, 'wb')
+end
+
+function tmux.open()
+  if is_open() then
+    return tmux.pane_id
+  end
+  if not tmux.post_buffer then
+    tmux.create()
+  end
   tmux_send({
     'split-window',
     tmux.horizontal and '-v' or '-h',
@@ -26,41 +51,56 @@ sclang.on_init:replace(function()
     tmux.size,
     'tail',
     '-F',
-    path,
+    tmux.path,
   })
   tmux.pane_id = get_pane_id()
   tmux_send({ 'last-pane' })
-end)
+  return tmux.pane_id
+end
 
-sclang.on_exit:replace(function()
-  if tmux.post_buffer then
-    tmux.post_buffer:close()
-  end
+function tmux.close()
   if tmux.pane_id then
     tmux_send({
       'kill-pane',
       '-t',
       tmux.pane_id
     })
+    tmux.pane_id = nil
   end
-end)
+end
 
-sclang.on_output:replace(function(line)
+function tmux.toggle()
+  if is_open() then
+    tmux.close()
+  else
+    tmux.open()
+  end
+end
+
+function tmux.destroy()
+  tmux.close()
+  if tmux.post_buffer then
+    tmux.post_buffer:close()
+  end
+end
+
+function tmux.post(line)
   if not tmux.post_buffer then
     return
   end
   tmux.post_buffer:write(line, '\n')
   tmux.post_buffer:flush()
-end)
-
--- overrides
-postwin.open = function()
-  tmux_send({
-    'select-pane',
-    '-t',
-    tmux.pane_id
-  })
 end
+
+-- Actions
+sclang.on_init:replace(tmux.open)
+sclang.on_exit:replace(tmux.destroy)
+sclang.on_output:replace(tmux.post)
+
+-- Overrides
+postwin.open = tmux.open
+postwin.close = tmux.close
+postwin.toggle = tmux.toggle
 
 return require'scnvim'.register_extension {
   setup = function(ext_config, user_config)
